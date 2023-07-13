ff14cd859484b213e7c84f4174975efcd04d9f8f
gpavankumarnov <90756309+gpavankumarnov@users.noreply.github.com>
2023-07-13 20:49:58 +0530

    added app.js file

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SlickSlider from './components/SlickSlider'
+
+const app = () => {
+  return (
+    <div>
+      <SlickSlider/>
+    </div>
+  )
+}
+
+export default app
